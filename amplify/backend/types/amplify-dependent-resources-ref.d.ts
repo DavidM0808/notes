5862b5d310a36de361b4cb7e978fe37f7c4e3269
DavidM0808 <majianxiang2002@gmail.com>
2024-08-19 17:06:56 -0400

    edited the files for the tutorial note application

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "notesapplication": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "notesapplication": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavBar, NoteUICollection, CreateNote, UpdateNote } from './ui-components';
+import { useState } from 'react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { DataStore } from 'aws-amplify/datastore';
 
-function App() {
+function App({ signOut }) {
+
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [updateNote, setUpdateNote] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <NavBar width="100%" marginBottom="20px" overrides={{
+        Button31632483: {onClick: () => {
+          setShowCreateModal(true)
+          console.log("Create notes button clicked")
+        }},
+        Button31632487: {onClick: async () => {
+          {
+            await DataStore.clear()
+            signOut()
+          }
+          console.log("Sign out button clicked")
+        }}
+      }} />
+
+      <div className='container'>
+      <NoteUICollection overrideItems={ ({item, idx}) => {
+        return {
+          overrides: {
+            Vector31472745: {
+              as: 'button', 
+              onClick: () => {
+                setShowUpdateModal(true)
+                setUpdateNote(item)
+              }
+            }
+          }
+        }
+      }} />
+
+      </div>
+
+      <div className='modal' style={{display: showCreateModal === false && 'none'}}>
+        <CreateNote overrides={{
+          MyIcon: {as: 'button', onClick: () => setShowCreateModal(false)}
+        }} />
+      </div>
+      
+      <div className='modal' style={{display: showUpdateModal === false && 'none'}}>
+        <UpdateNote notes={updateNote} overrides={{
+          MyIcon: {as: 'button', onClick: () => setShowUpdateModal(false)}
+        }}/>
+      </div>
+
+      {/* <NavBar width="100%" marginbottom="20px" />
+
+      <div className='container'>
+        <NoteUICollection />
+      </div>
+
+      <div className='modal' style={{display: 'none'}}>
+        <CreateNote />
+      </div>
+
+      <div className='modal' style={{display: 'none'}}>
+        <UpdateNote />
+      </div> */}
+
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
