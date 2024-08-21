f5a7e77440cdf3d38478d8f526430e7d26e466f6
DavidM0808 <majianxiang2002@gmail.com>
2024-08-21 14:59:07 -0400

    Attempted to adjust lambda function policy to comply with deployment

--- amplify/backend/function/notesCurdFunction/src/app.js
+++ amplify/backend/function/notesCurdFunction/src/app.js
@@ -7,6 +7,9 @@
 	AUTH_NOTESAPPLICATION_USERPOOLID
 	ENV
 	REGION
+	STORAGE_CRUDSAMPLE123_ARN
+	STORAGE_CRUDSAMPLE123_NAME
+	STORAGE_CRUDSAMPLE123_STREAMARN
 Amplify Params - DO NOT EDIT *//*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
