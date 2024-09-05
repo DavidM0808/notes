75bc30f85b17980730bb3bba71fad3544d4b857a
DavidM0808 <majianxiang2002@gmail.com>
2024-09-05 15:55:12 -0400

    Added a custom "createNote" operation that goes through the lambda function

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,22 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const createNote = /* GraphQL */ `
+  mutation CreateNote($input: CreateNoteInput!) {
+    createNote(input: $input) {
+      id
+      title
+      text
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
 export const createNotes = /* GraphQL */ `
   mutation CreateNotes(
     $input: CreateNotesInput!
