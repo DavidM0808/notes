230fe0a8dd36f3301872d1686abce5b14c9ccfe0
DavidM0808 <majianxiang2002@gmail.com>
2024-09-05 15:37:05 -0400

    Finished writing the logic for the lambda function backend. Have not tested it yet.

--- amplify/backend/function/notesLimitCheck/src/index.js
+++ amplify/backend/function/notesLimitCheck/src/index.js
@@ -5,14 +5,58 @@ const dynamoDB = new DynamoDBClient;
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    const { userId } = event.identity.claims; // This is the authenticated user ID (owner)
+    const { title, text } = event.arguments.input;
+
+    const tableName = process.env.STORAGE_Notesjwmvhzzwkbf7djbiu327z2gqoestaging_NAME;
+
+    // Check the notes limit for the owner.
+    try{
+
+        const params = {
+            TableName: tableName,
+            IndexName: "byOwner",
+            KeyConditionExpression: "owner = :owner",
+            ExpressionAttributeValues: {
+               ":owner" : userId
+            }
+        }
+
+        const data = await dynamoDB.query(params).promise();
+
+        // If the limit is greater than 5, throw exception.
+        if (data.Items.length >= 5) {
+            throw new Error('Limit exceeded: You can only create up to 5 notes.');
+        }
+        else { // Limit is not exceeded, we can create a new note entry.
+            const noteId = `note-${Date.now()}`; 
+
+            const newNote = {
+                id: noteId,
+                owner: userId,
+                title: title,
+                text: text,
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString(),
+            };
+    
+            const putParams = {
+                TableName: tableName,
+                Item: newNote
+            };
+    
+            await dynamoDB.put(putParams).promise();
+    
+            return newNote;
+        }
+
+
+    }
+
+    catch(error){
+
+        console.error("Error creating note: ", error);
+        throw new Error(error.message);
+
+    }
 };
